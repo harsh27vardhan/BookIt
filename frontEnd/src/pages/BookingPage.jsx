3927fe6d50e8acf0c71b81d561c03c05ae5b625e
Harsh Vardhan <reachvardhanharsh9@gmail.com>
2024-12-31 20:07:34 +0530

    more designing

--- frontEnd/src/pages/BookingPage.jsx
+++ frontEnd/src/pages/BookingPage.jsx
@@ -15,29 +15,32 @@ const BookingPage = () => {
     return (
         <div className='flex flex-col gap-4'>
             <AccountNav />
-            {bookings.length > 0 && bookings.map((booking, index) => (
-                <div key={index} className='booking-div flex gap-4 bg-gray-100 rounded-2xl overflow-hidden'>
-                    <div className='w-48'>
-                        <img src={booking.placeId.photos?.[0]} className='h-full' alt="" />
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+
+                {bookings.length > 0 && bookings.map((booking, index) => (
+                    <div key={index} className='booking-div flex gap-4 bg-gray-100 rounded-2xl overflow-hidden'>
+                        <div className='w-48'>
+                            <img src={booking.placeId.photos?.[0]} className='h-full' alt="" />
+                        </div>
+                        <div className='py-3 pr-3 grow'>
+                            <h2 id='booking-heading' className='text-2xl font-semibold'>{booking.placeId.title}</h2>
+                            <div className='booking-dates font-semibold border-t border-gray-300 mt-2 py-2'>
+                                <p>📆{format(new Date(booking.checkIn), "yyyy-MM-dd")}</p>
+                                <p>&rarr;</p>
+                                <p>📆{format(new Date(booking.checkOut), "yyyy-MM-dd")}</p>
+                            </div>
+                            <div className='booking-dates font-semibold text-xl'>
+                                🌙{differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} nights
+                            </div>
+                            <div className='booking-price font-semibold text-xl'>
+                                <p>💵Total Price :</p>
+                                <p>₹ {booking.price}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='py-3 pr-3 grow'>
-                        <h2 id='booking-heading' className='text-2xl font-semibold'>{booking.placeId.title}</h2>
-                        <div className='booking-dates font-semibold border-t border-black mt-2 py-2'>
-                            <p>📆{format(new Date(booking.checkIn), "yyyy-MM-dd")}</p>
-                            <p>&rarr;</p>
-                            <p>📆{format(new Date(booking.checkOut), "yyyy-MM-dd")}</p>
-                        </div>
-                        <div className='booking-dates font-semibold text-xl'>
-                            🌙{differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} nights
-                        </div>
-                        <div className='booking-price font-semibold text-xl'>
-                            <p>💵Total Price :</p>
-                            <p>₹ {booking.price}</p>
-                        </div>
-                    </div>
-                </div>
-            ))
-            }
+                ))
+                }
+            </div>
         </div >
     )
 }
